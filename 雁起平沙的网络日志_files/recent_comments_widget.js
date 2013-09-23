15bb47201f4cf5cd6a742414d1940f481b4df551
hanjinheng <hanjinheng@126.com>
2013-09-23 19:21:45 +0800

    a

---
+++ 雁起平沙的网络日志_files/recent_comments_widget.js
@@ -0,0 +1,79 @@
+
+
+document.write(' \
+<style type="text/css" media="screen">\
+	 .dsq-widget ul.dsq-widget-list {\
+	 padding: 0;\
+	 margin: 0;\
+	 text-align: left;\
+	 }\
+	 img.dsq-widget-avatar {\
+	 width: 32px;\
+	 height: 32px;\
+	 border: 0px;\
+	 margin: 0px;\
+	 padding: 0px 3px 3px 0px;\
+	 float: left;\
+	 }\
+	 a.dsq-widget-user {\
+	 font-weight: bold;\
+	 }\
+	 a.dsq-widget-thread {\
+	 font-weight: bold;\
+	 }\
+	 p.dsq-widget-meta {\
+	 clear: both;\
+	 font-size: 80%;\
+	 padding: 0;\
+	 margin: 0;\
+	 }\
+	 li.dsq-widget-item {\
+	 margin: 15px 0;\
+	 list-style-type: none;\
+	 clear: both;\
+	 }\
+	 span.dsq-widget-clout {\
+	 padding: 0 2px;\
+	 background-color: #ff7300;\
+	 color: #fff;\
+	 }\
+	 table.dsq-widget-horiz td {\
+	 padding-right: 15px;\
+	 }\
+	 .dsq-widget-comment p {\
+	 display: inline;\
+	 }\
+	 </style>\
+	 <ul class="dsq-widget-list">\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/ypchen/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/uploads/users/1922/4483/avatar92.jpg?1321601342"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/ypchen/">ypchen</a>\
+	 <span class="dsq-widget-comment"><p>把你的github账号发给我...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://yanping.me/cn/blog/2012/03/18/github-pages-step-by-step/">Github Pages极简教程 - 雁起平沙的网络日志</a>&nbsp;&middot;&nbsp;<a href="http://yanping.me/cn/blog/2012/03/18/github-pages-step-by-step/#comment-1054725967">21 hours ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/zemily/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/uploads/users/7433/5507/avatar92.jpg?1379853813"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/zemily/">zemily</a>\
+	 <span class="dsq-widget-comment"><p>你好，我现在在自己的机子上已经有了...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://yanping.me/cn/blog/2012/03/18/github-pages-step-by-step/">Github Pages极简教程 - 雁起平沙的网络日志</a>&nbsp;&middot;&nbsp;<a href="http://yanping.me/cn/blog/2012/03/18/github-pages-step-by-step/#comment-1054722838">21 hours ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/ypchen/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/uploads/users/1922/4483/avatar92.jpg?1321601342"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/ypchen/">ypchen</a>\
+	 <span class="dsq-widget-comment"><p>当然可以 你看一下我这几篇文章...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://yanping.me/cn/blog/2012/02/06/github-pages-domain-3/">三谈github页面域名绑定：域名跳转 - 雁起平沙的网络日志</a>&nbsp;&middot;&nbsp;<a href="http://yanping.me/cn/blog/2012/02/06/github-pages-domain-3/#comment-1054558163">1 day ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/lampmanyao/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/uploads/users/2123/1515/avatar92.jpg?1379817108"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/lampmanyao/">lampmanyao</a>\
+	 <span class="dsq-widget-comment"><p>想请教博主一个问题：我有两个域名，...</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://yanping.me/cn/blog/2012/02/06/github-pages-domain-3/">三谈github页面域名绑定：域名跳转 - 雁起平沙的网络日志</a>&nbsp;&middot;&nbsp;<a href="http://yanping.me/cn/blog/2012/02/06/github-pages-domain-3/#comment-1054427069">1 day ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="http://disqus.com/disqus_mxjSN4NUsh/"><img class="dsq-widget-avatar" src="//a.disquscdn.com/uploads/users/7133/8752/avatar92.jpg?1378967500"></a>\
+	 <a class="dsq-widget-user" href="http://disqus.com/disqus_mxjSN4NUsh/">Zhang</a>\
+	 <span class="dsq-widget-comment"><p>试试看</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://yanping.me/cn/blog/2013/02/13/generate-tags-with-js-in-jekyll-blog/">用js在jekyll博客中实现标签云和标签页</a>&nbsp;&middot;&nbsp;<a href="http://yanping.me/cn/blog/2013/02/13/generate-tags-with-js-in-jekyll-blog/#comment-1039762836">1 week ago</a></p>\
+	 </li>\
+	 </ul>\
+');
