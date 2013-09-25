09a75487847367735b9f5248a138570ea8fe2bbe
hanjinheng <hanjinheng@126.ccom>
2013-09-25 18:39:01 +0800

    las

--- 雁起平沙的网络日志_files/count-data.js
+++
@@ -1,5 +0,0 @@
-var DISQUSWIDGETS;
-
-if (typeof DISQUSWIDGETS != 'undefined') {
-    DISQUSWIDGETS.displayCount({"text":{"and":"\u53ca","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"uid":0,"comments":4},{"uid":1,"comments":3},{"uid":2,"comments":0},{"uid":3,"comments":3},{"uid":4,"comments":3},{"uid":5,"comments":5},{"uid":6,"comments":3}]});
-}
